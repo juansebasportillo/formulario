d8e25bde2dd548dd351d02fdbc35e00cfc6b5925
juansebasportillo <juansepor00@gmail.com>
2023-03-01 00:09:21 -0300

    Avance formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'Basic ' + btoa('skytel_form:oDXAoC6cos7ErfnxXRuHeFbLVWDy')
+  })
+};
+
+/*const basicAuth = 'skytel_form:oDXAoC6cos7ErfnxXRuHeFbLVWDy';
+let Headers = new HttpHeaders();
+Headers = Headers.append('Authorization', 'Basic ' + btoa(basicAuth));*/
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'formulario';
+  url = 'http://127.0.0.1:8000/api/formulario';
+
+  constructor(private http:HttpClient){}
+
+  onSubmit(data:any){
+    
+    this.http.post(this.url, data, httpOptions).subscribe((result)=>{
+      console.warn("result", result);
+    })
+
+    /*this.http
+    .post(this.url, { headers: Headers })
+    .subscribe(
+        data => { // json data
+            console.log('Success: ', data);
+        },
+        error => {
+            console.log('Error: ', error);
+        });*/
+
+    console.warn(data);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
+
+//Se importa el Forms Module
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
